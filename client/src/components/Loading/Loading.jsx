f2acc97c1212c8264705cb348d6cba8bd312e8ac
Okunola O Samson <68032187+pablo-clueless@users.noreply.github.com>
2021-12-01 15:09:40 +0100

    Pablo clueless (#10)
    * tours card done
    
    * error and loading components fixed

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,11 +1,11 @@
 import "./Loading.css"
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
 
-
-const Loading = () => {
+const Loading = ({loading}) => {
 
     return (
-        <div>
-            Loading page
+        <div className="box">
+            <ClimbingBoxLoader color={"hsl(205, 63%, 48%)"} loading={loading} size={40}/>
         </div>
     )
 }
--- client/src/components/Tours/Tours.jsx
+++ client/src/components/Tours/Tours.jsx
@@ -1,59 +1,59 @@
 import { useEffect, useState } from 'react';
+import Loading from '../Loading/Loading';
+import Error from '../Error/Error';
 import './Tours.css'
 
 const url = "https://course-api.com/react-tours-project";
 const Tours = () => {
 
     const [tours, setTours] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false)
 
     const fetchTours = async () => {
             const requestTours = await fetch(url);
             const resp = await requestTours.json();
             setTours(resp);
+            setLoading(false)
     }
 
     useEffect(()=>{
         fetchTours();
     }, []);
+
+    if(loading) {
+        return(
+            <Loading loading={loading}/>
+        )
+    }
+
+    if(error) {
+        return(
+            <Error />
+        )
+    }
+
     return (
         <main>
-            <div className="card">
-                <img src="https://images.unsplash.com/photo-1637921986514-fdb90068fbf8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80" alt="" />
-                <div className="info">
-                <h1 className='title'>Tour de France</h1>
-                <p className='content'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut animi corporis repellat saepe neque, pariatur quibusdam enim, explicabo a iusto totam delectus non, voluptatem quia iure voluptatum mollitia placeat atque. Fuga esse impedit id autem sunt totam voluptatum corrupti, quae ex eveniet omnis consequuntur quas fugit iure dolores qui officia.
-                </p>
-                <div className='price'>
-                    <p>$120.00</p>
-                    <button>Check In</button>
-                </div>
-                </div>
-            </div>
+            {tours.map((tour) => {
 
-            
-                {tours.map((tour) => {
-
-                    const {id,name,info,image,price} = tour;
-                    return(
-                            <div className="card" key={id}>
-                            <img src={image} alt={name} />
-                            <div className="info">
-                            <h1 className='title'>{name}</h1>
-                            <p className='content'>
-                                {info}
-                            </p>
-                            <div className='price'>
-                                <p>{price}</p>
-                                <button>Check In</button>
-                            </div>
-                            </div>
-                            </div>
-                    )
-                })}
-                
-    
-
+                const {id,name,info,image,price} = tour;
+                return(
+                        <div className="card" key={id}>
+                        <img src={image} alt={name} />
+                        <div className="info">
+                        <h1 className='title'>{name}</h1>
+                        <p className='content'>
+                            {info}
+                        </p>
+                        <div className='price'>
+                            <p>$ {price}</p>
+                            <button onClick={() => setError(true)}>Not Interested</button>
+                        </div>
+                        </div>
+                        </div>
+                )
+            })}             
         </main>
     )
 }
