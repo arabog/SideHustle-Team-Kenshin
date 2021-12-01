b2f4375701824d08fba5b165e46b99795a8b7710
iCode_Rx <arababs2015@gmail.com>
2021-12-01 14:45:44 +0100

    show More Less Button by HUGB77e

--- client/src/components/Tour/Tour.jsx
+++ client/src/components/Tour/Tour.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import styled from "styled-components"
 
+import { useState } from "react"
+
+
+const Button = styled.button `
+          display: inline-block;
+          cursor: pointer;
+          margin-left: 10px;
+          background: hsl(205, 78%, 60%);
+          color: white;
+          padding: 5px 10px;
+
+          border: none;
+          outline: none;
+`
 
 const Tour = ( { tour } ) => {
-          const handleClick = (e) => {
-                    e.preventDefault()
+          const [showMore, setShowMore] = useState(false);
 
+          const { info } = tour;
                     
-          }
-
 
           return (
-                    <div className="card" onClick={handleClick}>
+                    <div className="card" >
                               <img src={tour.image} alt="" />
 
                               <div className="info">
                                         <h1 className='title'> {tour.name} </h1>
                                         
-                                        <p className='content'> {tour.info} </p>
+                                        <div className='content'>  
+                                                  {
+                                                            showMore ? info : `${info.substring(0, 180)}`
+                                                  }                                                  
+                                                  
+                                                  <Button onClick={() => setShowMore(!showMore)}>
+                                                            {showMore ?  "Show Less" : "Show More" }
+                                                  </Button>
+                                        </div>
                                         
                                         <div className='price'>
                                                   <p>${tour.price}</p>
+
                                                   <button>Check In</button>
                                         </div>
                               </div>
